ca9df615641fa5a7025bab651e6a604d0bfdaaf1
SantiagoBreton <128427309+SantiagoBreton@users.noreply.github.com>
2024-10-03 13:15:00 -0300

    Refactor app/index.tsx to use react-native-maps for displaying a map

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
-import { Contador } from "@/components/Contador";
-import Tarjeta from "@/components/Tarjeta";
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export default function Index() {
   return (
-
-    
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-    <Contador />
-    <Tarjeta/>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
