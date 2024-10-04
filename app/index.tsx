a96a3128f3f8f4e9461a577693efd10f672d13df
MatiasdeBuren <64704995+MatiasdeBuren@users.noreply.github.com>
2024-10-04 17:23:46 -0300

    Cambie el mapa para que este centrado en el usuario, muestra la posicion en todo momento.

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+import { ActivityIndicator } from 'react-native';
 
 export default function Index() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      {location && (
+      <MapView
+      style={styles.map}
+      showsUserLocation={true}
+      initialRegion={{
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005,
+      }}
+      userLocationAnnotationTitle="You are here"
+      customMapStyle={[
+      {
+        elementType: 'geometry',
+        stylers: [{ visibility: 'simplified' }],
+      },
+      {
+        elementType: 'labels.icon',
+        stylers: [{ visibility: 'off' }],
+      },
+      ]}
+      />
+      )}
     </View>
   );
 }
