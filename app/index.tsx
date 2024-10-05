242577dd460efe0edb025d128f0d9ff171820324
MatiasdeBuren <64704995+MatiasdeBuren@users.noreply.github.com>
2024-10-05 12:15:06 -0300

    .

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
-import { StyleSheet, View, Text, Modal} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
 import { ActivityIndicator } from 'react-native';
 
 export default function Index() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
-  const [modalVisible, setModalVisible] = useState(false);
   const [loading, setLoading] = useState(true);
   const [events, setEvents] = useState([
     // Simulación de eventos con coordenadas e información
@@ -38,15 +37,6 @@ export default function Index() {
       setLoading(false);
     })();
   }, []);
-
-  useEffect(() => {
-    if (!loading) {
-      const timer = setTimeout(() => {
-        setModalVisible(true);
-      }, 3000);
-      return () => clearTimeout(timer);
-    }
-  }, [loading]);
   
   if (loading) {
     return <ActivityIndicator size="large" color="#0000ff" />;
@@ -54,19 +44,6 @@ export default function Index() {
   
   return (
     <View style={styles.container}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.modalView}>
-          <Text style={styles.modalTitle}>Modal Title</Text>
-          <Text>Modal Content</Text>
-        </View>
-      </Modal>
       {location && (
         <MapView
           style={styles.map}
@@ -118,24 +95,5 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: 'bold',
     fontSize: 16,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  modalTitle: {
-    fontWeight: 'bold',
-    fontSize: 18,
   },
 });
