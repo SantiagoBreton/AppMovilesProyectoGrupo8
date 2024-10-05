283cf6f7c5cfec49fdaa7301a9d4311e1ff7b05a
MatiasdeBuren <64704995+MatiasdeBuren@users.noreply.github.com>
2024-10-05 20:16:26 -0300

    Sacar el teclado al tocar fuera.

--- app/inicio_perfil.tsx
+++ app/inicio_perfil.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Animated, Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 export default function InicioPerfil() {
     const [isLogin, setIsLogin] = useState(true);
@@ -42,53 +42,55 @@ export default function InicioPerfil() {
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{isLogin ? 'Iniciar Sesión' : 'Registro'}</Text>
-            <Animated.View style={{ transform: [{ scale: emailAnimation }] }}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Correo Electrónico"
-                    value={email}
-                    onChangeText={setEmail}
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                    placeholderTextColor="#A9A9A9"
-                    onFocus={() => bounceAnimation(emailAnimation)} //bouncea cunado clicekas en el input
-                />
-            </Animated.View>
-            <Animated.View style={{ transform: [{ scale: passwordAnimation }] }}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Contraseña"
-                    value={password}
-                    onChangeText={setPassword}
-                    secureTextEntry
-                    placeholderTextColor="#A9A9A9"
-                    onFocus={() => bounceAnimation(passwordAnimation)} //bouncea cuanco clickeas en el input
-                />
-            </Animated.View>
-            {!isLogin && (
-                <Animated.View style={{ transform: [{ scale: confirmPasswordAnimation }] }}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+                <Text style={styles.title}>{isLogin ? 'Iniciar Sesión' : 'Registro'}</Text>
+                <Animated.View style={{ transform: [{ scale: emailAnimation }] }}>
                     <TextInput
                         style={styles.input}
-                        placeholder="Confirmar Contraseña"
-                        value={confirmPassword}
-                        onChangeText={setConfirmPassword}
-                        secureTextEntry
+                        placeholder="Correo Electrónico"
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
                         placeholderTextColor="#A9A9A9"
-                        onFocus={() => bounceAnimation(confirmPasswordAnimation)} // bouncea inpt
+                        onFocus={() => bounceAnimation(emailAnimation)} //bouncea cuando clicekas en el input
                     />
                 </Animated.View>
-            )}
-            <TouchableOpacity style={styles.button} onPress={handleAuth}>
-                <Text style={styles.buttonText}>{isLogin ? 'Iniciar Sesión' : 'Registrar'}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setIsLogin(!isLogin)}>
-                <Text style={styles.toggleText}>
-                    {isLogin ? '¿No tienes una cuenta? Regístrate' : '¿Ya tienes una cuenta? Inicia Sesión'}
-                </Text>
-            </TouchableOpacity>
-        </View>
+                <Animated.View style={{ transform: [{ scale: passwordAnimation }] }}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Contraseña"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                        placeholderTextColor="#A9A9A9"
+                        onFocus={() => bounceAnimation(passwordAnimation)} //bouncea cuando clickeas en el input
+                    />
+                </Animated.View>
+                {!isLogin && (
+                    <Animated.View style={{ transform: [{ scale: confirmPasswordAnimation }] }}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Confirmar Contraseña"
+                            value={confirmPassword}
+                            onChangeText={setConfirmPassword}
+                            secureTextEntry
+                            placeholderTextColor="#A9A9A9"
+                            onFocus={() => bounceAnimation(confirmPasswordAnimation)} // bouncea inpt
+                        />
+                    </Animated.View>
+                )}
+                <TouchableOpacity style={styles.button} onPress={handleAuth}>
+                    <Text style={styles.buttonText}>{isLogin ? 'Iniciar Sesión' : 'Registrar'}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setIsLogin(!isLogin)}>
+                    <Text style={styles.toggleText}>
+                        {isLogin ? '¿No tienes una cuenta? Regístrate' : '¿Ya tienes una cuenta? Inicia Sesión'}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
