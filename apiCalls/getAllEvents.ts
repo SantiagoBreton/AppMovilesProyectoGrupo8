349f5bac1bdf8064a136715f079a803eddbc6f51
SantiagoBreton <bretonsantiago87@gmail.com>
2024-11-20 20:11:00 -0300

    Update dependencies and enhance event fetching with useFocusEffect

--- apiCalls/getAllEvents.ts
+++ apiCalls/getAllEvents.ts
@@ -1,10 +1,41 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import { SERVER_IP } from '@env';
 
 export const allEvents = (trigger: boolean) => {
   const [events, setEvents] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [eventsError, setEventsError] = useState<string | null>(null);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchEvents = async () => {
+        try {
+          const response = await fetch(`http://${SERVER_IP}:3000/getEvents`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+  
+          if (!response.ok) {
+            throw new Error('Failed to get events');
+          }
+  
+          const fetchedEvents = await response.json();
+          setEvents(fetchedEvents); // Set the fetched events to state
+        } catch (error) {
+          setEventsError('Error fetching events');
+          console.error('Error fetching events:', error);
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchEvents();
+    }, [trigger])
+  );
+
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -32,6 +63,9 @@ export const allEvents = (trigger: boolean) => {
 
     fetchEvents();
   }, [trigger]);
+
+
+
 
   return { events, loading, eventsError };
 };
