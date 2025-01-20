21d7cb2c44d2b2cac928987ad6733f2d5ec75f18
MatiasdeBuren <64704995+MatiasdeBuren@users.noreply.github.com>
2025-01-20 18:55:46 -0300

    Another day, another victory royale ~Kevin

--- apiCalls/getAllEvents.ts
+++ apiCalls/getAllEvents.ts
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useFocusEffect } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { useFocusEffect } from 'expo-router';
 
-
-export const allEvents = (trigger: boolean) => {
+export const useAllEvents = (trigger: boolean) => {
   const [events, setEvents] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [eventsError, setEventsError] = useState<string | null>(null);
@@ -10,6 +9,8 @@ export const allEvents = (trigger: boolean) => {
   useFocusEffect(
     React.useCallback(() => {
       const fetchEvents = async () => {
+        setLoading(true); // Reset loading state on focus
+        setEventsError(null); // Clear previous error
         try {
           const response = await fetch(`http://${process.env.EXPO_PUBLIC_SERVER_IP}:3000/getEvents`, {
             method: 'GET',
@@ -17,55 +18,24 @@ export const allEvents = (trigger: boolean) => {
               'Content-Type': 'application/json',
             },
           });
-  
+
           if (!response.ok) {
             throw new Error('Failed to get events');
           }
-  
+
           const fetchedEvents = await response.json();
           setEvents(fetchedEvents); // Set the fetched events to state
         } catch (error) {
-          setEventsError('Error fetching eventsdasda');
-          console.error('Error fetching eventsdasd:', error);
+          setEventsError('Error fetching events');
+          console.error('Error fetching events:', error);
         } finally {
           setLoading(false);
         }
       };
-  
+
       fetchEvents();
     }, [trigger])
   );
-
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const response = await fetch(`http://${process.env.SERVER_IP}:3000/getEvents`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error('Failed to get events');
-        }
-
-        const fetchedEvents = await response.json();
-        setEvents(fetchedEvents); // Set the fetched events to state
-      } catch (error) {
-        setEventsError('Error fetching events');
-        console.error('Error fetching events:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchEvents();
-  }, [trigger]);
-
-
-
 
   return { events, loading, eventsError };
 };
